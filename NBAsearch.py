776f1e6535326ab6c91fb5d8b6cbc255889765f9
miguel-mach-eight <83597928+miguel-mach-eight@users.noreply.github.com>
2021-05-03 10:44:44 -0500

    Update NBAsearch.py
    Improved the naive implementation of the initial version.

--- NBAsearch.py
+++ NBAsearch.py
@@ -7,12 +7,8 @@ Created on Mon Apr  5 16:30:22 2021
 
 import json
 import requests
+import collections
 
-def to_number(A):
-    
-    B = int(A)
-    
-    return B
         
 
 def search(Number):
@@ -21,28 +17,17 @@ def search(Number):
     data = json.loads(response.text)
     PLAYERS = data["values"]
 
-    answer_list = []
+    d = collections.defaultdict(list)
+    for player in players:
+        d[int(player['h_in'])].append(player['first_name'] + " " + player['last_name'])
 
-    for player1 in PLAYERS:
-    
-        raw_height = player1['h_in']
-        height_1 = to_number(raw_height)
-    
-        PLAYERS.remove(player1)
-        for player2 in PLAYERS:
-        
-            raw_height = player2['h_in']
-            height_2 = to_number(raw_height)
-        
-            result = height_1 + height_2
-        
-            if result == Number:
-                par = (player1['first_name'] + ' ' + player1['last_name'] + ' & ' + 
-                       player2['first_name'] + ' ' + player2['last_name'])
-            
-                answer_list.append(par)
-        
-    return answer_list
+    return [player + " & " + other
+        for height in d
+            if total - height in d
+                for other in d[total - height]
+                    for player in d[height]
+                        if player < other
+    ]
 
 def executer():
     Number = int(input("insert your integer: "))
