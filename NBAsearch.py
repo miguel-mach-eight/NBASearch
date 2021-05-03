7de4c1295dad934eb2a6e4f10ce62c6ee871ecb3
miguel-mach-eight <83597928+miguel-mach-eight@users.noreply.github.com>
2021-05-03 11:19:23 -0500

    Update NBAsearch.py
    Improvement from last algorithm using dictionaries to a Binary Search Algorithm, with great results. For example: diminishing the run time for an integer input of 140 from 0.038065433502197266 s  (with my original code) to  0.0009987354278564453 s

--- NBAsearch.py
+++ NBAsearch.py
@@ -7,30 +7,70 @@ Created on Mon Apr  5 16:30:22 2021
 
 import json
 import requests
-
 import collections
-
-
-        
+import time
 
 def search(Number):
     response = requests.get("https://mach-eight.uc.r.appspot.com/")
 
     data = json.loads(response.text)
     PLAYERS = data["values"]
-
-
-    d = collections.defaultdict(list)
-    for player in players:
-        d[int(player['h_in'])].append(player['first_name'] + " " + player['last_name'])
-
-    return [player + " & " + other
-        for height in d
-            if total - height in d
-                for other in d[total - height]
-                    for player in d[height]
-                        if player < other
-    ]
+    
+    PLAYERS = sorted(PLAYERS, key=lambda k: k['h_in'])
+    
+    lo, hi = 0, len(PLAYERS) - 1
+    
+    answer_list = []
+    
+    special_case = False
+    
+    start_time = time.time()
+    
+    while lo < hi:
+        
+        player1 = PLAYERS[lo]
+        player2 = PLAYERS[hi]
+        player3 = PLAYERS[lo+1]
+        player4 = PLAYERS[hi-1]
+        
+        total = int(player1['h_in']) + int(player2['h_in'])
+        
+        if total == Number:
+            pair = (player1['first_name'] + ' ' + player1['last_name'] + ' & ' + 
+                       player2['first_name'] + ' ' + player2['last_name'])
+            
+            answer_list.append(pair)  
+            
+            if special_case == True:
+                hi += 1
+                lo += 1
+                special_case == False
+                
+            if player1['h_in'] == player3['h_in']:
+                if player2['h_in'] == player4['h_in']:
+                    special_case = True
+                    hi -= 1
+                    
+            elif player1['h_in'] == player3['h_in']:
+                lo += 1
+            
+            elif player2['h_in'] == player3['h_in']:
+                hi -= 1
+            
+            else:
+                lo += 1
+                hi -= 1
+        
+        elif total < Number:
+            lo += 1
+        
+        else:
+            hi -= 1
+    
+    elapsed_time = time.time() - start_time
+    print('elapsed time = ', elapsed_time, 's')
+    
+    return answer_list
 
 
 def executer():
